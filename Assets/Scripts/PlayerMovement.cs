a6ee5f68668785eceb7ab02c019ec329ea51d1b5
Diego Cavalcante <cavalcante13.santos@gmail.com>
2017-11-16 01:09:56 -0200

    First Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rigidyBody;
+
+	public float forwardForce = 1000f;
+	public float sidewaysForce = 500f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		rigidyBody.AddForce (0, 0, forwardForce * Time.deltaTime);
+
+
+		if (Input.GetKey ("d")) {
+			rigidyBody.AddForce (sidewaysForce * Time.deltaTime, 0, 0);	
+		} else if (Input.GetKey ("a")) {
+			rigidyBody.AddForce (-sidewaysForce * Time.deltaTime, 0, 0);	
+		}
+	}
+}
